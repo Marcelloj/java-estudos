b26159b8bdf6b180e41fb8a0c372f47439c1ea6f
Marcello Júnior <celojuniorii2003@gmail.com>
2026-07-16 23:53:26 -0300

    Estrutura repetitiva WHILE e exercicios

---
+++ estrutura_repetitiva_while/src/teste_exercico_mesa.java
@@ -0,0 +1,18 @@
+
+public class teste_exercico_mesa {
+	
+	public static void main(String[] args) {
+		
+	int x = 2;
+	int y = 10;
+	System.out.println("Olá");
+	
+	while ( x < y ) {
+		System.out.println(x + "-" + y);
+		x = x * 2;
+		y = y + 1;
+		
+	}
+	}
+
+}
